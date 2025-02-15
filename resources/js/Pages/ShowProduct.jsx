c705e6640fe96a3ab073ce860de7c55a57dc8395
miraz66 <mirajulislam0509@gmail.com>
2025-02-16 01:17:17 +0600

    Show product or details

--- resources/js/Pages/ShowProduct.jsx
+++ resources/js/Pages/ShowProduct.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import Header from "@/Components/Header";
+import { Head } from "@inertiajs/react";
+import React, { useState } from "react";
 
 export default function ShowProduct() {
-    return <div>ShowProduct</div>;
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    return (
+        <div className="bg-slate-900">
+            <Head title="Blog" />
+
+            <Header
+                mobileMenuOpen={mobileMenuOpen}
+                setMobileMenuOpen={setMobileMenuOpen}
+            />
+
+            <div className="py-40 max-w-3xl mx-auto">
+                <h1 className="text-4xl font-open uppercase text-gray-50 tracking-wider">
+                    Sit voluptatem rhoncus
+                </h1>
+            </div>
+        </div>
+    );
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/blog', function () {
     return inertia('Blog');
 });
 
+Route::get('/show', function () {
+    return inertia('ShowProduct');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
